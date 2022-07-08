a821e3b7665fda082031630b381aaf40d493c231
Roman Kordanets <kordanets@mail.ru>
2022-07-08 17:36:03 +0300

    commit lesson 10

---
+++ task028/Program.cs
@@ -0,0 +1,23 @@
+﻿// Есть монотонная последовательность, каждое число встречается ровно то количество раз,
+// какое значение этого числа: 1, 2, 2, 3, 3, 3, 4, 4, 4, 4,... Выведите ряд этой последовательности
+// до N-го значения, желательно использовать рекурсию: N = 12 -> "1, 2, 2, 3, 3, 3, 4, 4, 4, 4, 5, 5"
+
+int Prompt(string message)
+{
+    Console.Write(message);
+    return int.Parse(Console.ReadLine());
+}
+
+void Recursion(int n)
+{
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 0; j < i; j++)
+        {
+            System.Console.Write($"{i} ");
+        }
+    }
+    return;
+}
+int n = Prompt("введите n => ");
+Recursion(n);
---
+++ task029/Program.cs
@@ -0,0 +1,30 @@
+﻿//  Напишите программу вычисления функции Аккермана с помощью рекурсии.
+//  Даны два неотрицательных числа m и n.     m = 3, n = 2 -> A(m,n) = 29
+
+int Prompt(string message)
+{
+    Console.Write(message);
+    return int.Parse(Console.ReadLine());
+}
+int Akkerman(int m, int n)
+{
+    if (m == 0)
+    {
+        return n + 1;
+    }
+    else
+    {
+        if (n == 0 && m > 0)
+        {
+            return Akkerman(m - 1, 1);
+        }
+        else
+        {
+            return Akkerman(m - 1, Akkerman(m, n - 1));
+        }
+    }
+}
+
+int m = Prompt("Введите m >");
+int n = Prompt("Введите n >");
+System.Console.WriteLine(Akkerman(m, n));
