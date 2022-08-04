087e865ee01297249c2f4a21d11e1c53e1002efe
Roman Kordanets <kordanets@mail.ru>
2022-08-04 13:47:36 +0300

    sorting

---
+++ meExamples/selection sorting/Program.cs
@@ -0,0 +1,19 @@
+﻿// Сортировка выбором (почти не применяется)
+
+int[] array = { 9, 4, 8, 2, 3 };
+Console.WriteLine("Начальный массив: [" + string.Join(", ", array) + "]");   // Join = соединить
+// Cортировка
+for (int i = 0; i < array.Length; i++)
+{
+    int MinIndex = i;
+    for (int j = i + 1; j < array.Length; j++)
+    {
+        if (array[j] < array[MinIndex])
+            MinIndex = j;
+    }
+    int temp;
+    temp = array[MinIndex];
+    array[MinIndex] = array[i];
+    array[i] = temp;
+}
+Console.WriteLine("Конечный массив: [" + string.Join(", ", array) + "]");
